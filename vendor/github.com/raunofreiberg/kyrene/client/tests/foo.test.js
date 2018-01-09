7a187e8bbba41d4bd380690d8426c76743f98599
Rauno <freiberggg@gmail.com>
2018-01-09 19:25:34 +0200

    remove local dependencies from vendor

--- vendor/github.com/raunofreiberg/kyrene/client/tests/foo.test.js
+++
@@ -1,5 +0,0 @@
-describe('#foo', () => {
-    it('passes', () => {
-        console.log('hello world');
-    });
-});
--- vendor/github.com/raunofreiberg/kyrene/webpack.config.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV !== 'production') {
-    module.exports = require('./webpack/webpack.dev.js');
-} else {
-    module.exports = require('./webpack/webpack.prod.js');
-}
