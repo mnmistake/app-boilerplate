371478c7dcf3eed0382476d6441cb32a24a796ce
Rauno <freiberggg@gmail.com>
2018-03-24 23:49:28 +0200

    feat: use apollo-link-error for grabbing errors

--- client/apollo.js
+++ client/apollo.js
@@ -3,7 +3,9 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 
+import history from './history';
 import stateLink from './state';
 import Auth from './utils/Auth';
 
@@ -23,8 +25,20 @@ const authLink = setContext((req, { headers }) => {
     };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+        graphQLErrors.map(({ message }) => {
+            if (message === 'Invalid token') {
+                Auth.removeToken();
+                history.push('/login');
+            }
+        });
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 const client = new ApolloClient({
     link: ApolloLink.from([
+        errorLink,
         authLink,
         stateLink(cache),
         httpLink,
