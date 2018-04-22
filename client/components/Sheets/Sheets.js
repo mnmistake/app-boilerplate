e442a8ee2607b6551b0661469a608ac8072cff5f
Rauno <freiberggg@gmail.com>
2018-04-22 16:40:40 +0300

    refactor: functional component for Sheets

--- client/components/Sheets/Sheets.js
+++ client/components/Sheets/Sheets.js
@@ -1,6 +1,5 @@
 // @flow
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
@@ -19,46 +18,36 @@ type Props = {
   }
 };
 
-@graphql(sheetsQuery)
-export default class Sheets extends React.Component<Props> {
-    static propTypes = {
-        data: PropTypes.shape({
-            sheets: PropTypes.array,
-            loading: PropTypes.bool.isRequired,
-        }).isRequired,
-    };
-
-    render() {
-        const { sheets, loading } = this.props.data;
-
-        if (loading) {
-            return <Spinner />;
-        }
-        
-        return (
-            <div className={classNames('container', styles.sheetsWrapper)}>
-                {sheets && sheets.map((sheet, idx) => {
-                    const isLastSheet = sheets.length - 1 === idx;
-                    const { id, name, createdAt, user: { username } } = sheet;
-
-                    return (
-                        <Fragment>
-                            <Link to={`/sheet/${id}`} className={styles.sheet} title={name}>
-                                <div className={styles.sheetDetails}>
-                                    <h1>{name}</h1>
-                                    <p className="note">Created {moment(createdAt).fromNow()}</p>
-                                </div>
-                                <Avatar username={username} />
-                            </Link>
-                            {isLastSheet &&
-                                <Link to={`/create`} className={styles.sheet}>
-                                    Create your sheet...
-                                </Link>
-                            }
-                        </Fragment>
-                    )
-                })}
-            </div>
-        );
+const Sheets = ({ data: { sheets, loading } }: Props) => {
+    if (loading) {
+        return <Spinner />;
     }
-}
+
+    return (
+        <div className={classNames('container', styles.sheetsWrapper)}>
+            {sheets && sheets.map((sheet, idx) => {
+                const isLastSheet = sheets.length - 1 === idx;
+                const { id, name, createdAt, user: { username } } = sheet;
+
+                return (
+                    <Fragment key={id}>
+                        <Link to={`/sheet/${id}`} className={styles.sheet} title={name}>
+                            <div className={styles.sheetDetails}>
+                                <h1>{name}</h1>
+                                <p className="note">Created {moment(createdAt).fromNow()}</p>
+                            </div>
+                            <Avatar username={username} />
+                        </Link>
+                        {isLastSheet &&
+                        <Link to={`/create`} className={styles.sheet}>
+                            Create your sheet...
+                        </Link>
+                        }
+                    </Fragment>
+                )
+            })}
+        </div>
+    );
+};
+
+export default graphql(sheetsQuery)(Sheets);
